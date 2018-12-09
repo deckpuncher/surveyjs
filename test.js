53bf76fcbe6654792a8499f9835b57edf2e34400
john <john@email.com>
2018-12-09 12:35:27 +1100

    more

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
-function Question(heading, prompt, isYesNo) {
+function Question(heading, prompt, category, isYesNo) {
     this.heading = heading;
     this.prompt = prompt;
+    this.category = category;
     this.isYesNo = isYesNo;
     this.statements = [];
     this.rules = [];
     this.answer = null;
+    this.priority = null;
 
     this.addStatement = function (statement) {
         this.statements.push(statement);
@@ -58,28 +60,37 @@ function Survey() {
 }
 
 function initSurvey() {
-    var q1 = new Question("Your Product", "Of these statements, how many can you confidently answer YES to?");
+    var q1 = new Question("Question 1", "Of these statements, how many can you confidently answer YES to?", "VISION");
     q1.addStatement("I have this thing");
     q1.addStatement("I have this other thing");
     q1.addStatement("I do this thing");
+    q1.priority = 1;
 
     q1.addRule(2, "you should visit this link");
     survey.questions.push(q1);
 
-    var q2 = new Question("2Your Product", "2Of these statements, how many can you confidently answer YES to?");
+    var q2 = new Question("Question 2", "2Of these statements, how many can you confidently answer YES to?", "WHY");
     q2.addStatement("2I have this thing");
     q2.addStatement("2I have this other thing");
     q2.addStatement("2I do this thing");
+    q2.priority = 1;
 
     q2.addRule(2, "you should visit this link2");
     survey.questions.push(q2);
 
-    var q3 = new Question("Important question", "This is the first yes/no?", true);
-    q3.addRule(0, "This is super important");
-    survey.yesNoQuestions.push(q3);
+    var q3 = new Question("Question 3", "Of these statements, how many can you confidently answer YES to?", "CUSTOMER");
+    q3.addStatement("2I have this thing");
+    q3.addStatement("2I have this other thing");
+    q3.addStatement("2I do this thing");
+    q3.priority = 2;
+    survey.questions.push(q3);
 
-    var q4 = new Question("Other impoartant question", "This is the second yes/no?", true);
-    survey.yesNoQuestions.push(q4);
+    var q4 = new Question("Question 4", "Of these statements, how many can you confidently answer YES to?", "PRODUCTS");
+    q4.addStatement("2I have this thing");
+    q4.addStatement("2I have this other thing");
+    q4.addStatement("2I do this thing");
+    q4.priority = 3;
+    survey.questions.push(q4);
 
     renderSurvey(survey);
 }
@@ -139,22 +150,45 @@ function answerClicked(button) {
     var questionNum = parent.getAttribute("survey-question-num");
     var rule = survey.questions[questionNum].recordAnswer(button.value);
     renderAnswerResponse(rule, parent);
-   
+
     if (survey.completed()) {
         var table = document.querySelector("#survey-results");
         if (table) {
             table.parentNode.removeChild(table);
         }
+        var questions = survey.questions.sort(compare);
+
         var tableRows = [];
-        survey.questions.forEach(function (question, index) {
-            tableRows.push("<tr>");
-            tableRows.push("<td>");
-            tableRows.push("Question ");
-            tableRows.push(index + 1);
-            tableRows.push("</td><td>");
-            tableRows.push(question.answer);
+        for (i = 0; i++; i < questions.length) {
+
+        }
+
+        var priority = null;
+        var closeRow = false;
+
+        questions.forEach(function (question, index) { 
+            var warning = question.answer < (question.statements.length / 2);
+            if (priority !== question.priority) {
+                priority = question.priority;
+
+                tableRows.push("<tr>");
+            }
+            else {
+                closeRow = true;
+            }
+
+            tableRows.push("<td ");
+            if (warning){
+                tableRows.push("class='warning'");
+            }
+            tableRows.push(">");
+            tableRows.push(question.category);
             tableRows.push("</td>");
-            tableRows.push("</tr>");
+
+            if (closeRow) {
+                tableRows.push("</tr>");
+                closeRow = false;
+            }
         });
 
         var container = document.body.querySelector("#survey-container");
@@ -162,7 +196,7 @@ function answerClicked(button) {
     }
 }
 
-function renderAnswerResponse(rule, parent){
+function renderAnswerResponse(rule, parent) {
     var answer = parent.querySelector(".survey-answer-statement");
     if (answer) {
         answer.parentNode.removeChild(answer);
@@ -185,6 +219,14 @@ function yesNoAnswerClicked(button) {
         var parent = button.closest(".survey-question");
         renderAnswerResponse(rule, parent)
     }
+}
+
+function compare(a, b) {
+    if (a.priority < b.priority)
+        return -1;
+    if (a.priority > b.priority)
+        return 1;
+    return 0;
 }
 
 function buildTag(tag, value, attributes) {
